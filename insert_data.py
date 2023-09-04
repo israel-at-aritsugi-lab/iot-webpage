277c7f8ae8b27d62ca613d09376b237498fdd9a1
Angeline0210 <angelinechong0210@gmail.com>
2023-09-04 14:27:17 +0900

    added comments

--- insert_data.py
+++ insert_data.py
@@ -2,13 +2,14 @@ from angelinedb import database
 import mongoengine
 import datetime
 import random
-from datetime import timedelta
+#from datetime import timedelta
 
 class sensorData(mongoengine.Document):
     sensor_uid = mongoengine.StringField()
     value = mongoengine.FloatField()
     timestamp = mongoengine.DateTimeField()
 
+#to keep the data updated
 def clean_database():
     sensorData.objects().delete()
 
@@ -23,7 +24,7 @@ def generate_dummy_data(num_entries):
         ]
         random_sensor_uid=random.choice(random.choice(ranges))  #random element in a given range
         random_value=round(random.uniform(0,99),2)   #float in a given range
-        random_timestamp=datetime.datetime(2023,9,random.randint(1,3),random.randint(0,23),random.randint(0,59),random.randint(0,59))  #int in a given range
+        random_timestamp=datetime.datetime(2023,9,random.randint(1,4),random.randint(0,23),random.randint(0,59),random.randint(0,59))  #int in a given range
         data_insert.append(sensorData(sensor_uid=str(random_sensor_uid), value=random_value, timestamp=random_timestamp))
     return data_insert
 
@@ -73,35 +74,7 @@ def generate_dummy_data(num_entries):
 
         #random_timestamp=datetime.datetime(2023,8,random.randint(1,31),random.randint(0,23),random.randint(0,59),random.randint(0,59))  #int in a given range
             
-
-
-    
-    data_insert = [
-        sensorData(sensor_uid="10103", value=20.75, timestamp=datetime.datetime(2023, 8, 23, 18, 0)),  #10103-> 20.75
-        sensorData(sensor_uid="10103", value=44.44, timestamp=datetime.datetime(2022, 6, 11, 12, 0)),  
-
-        sensorData(sensor_uid="30201", value=19.13, timestamp=datetime.datetime(2023, 8, 23, 14, 0)),  #30201-> 19.13
-        sensorData(sensor_uid="30201", value=4.75, timestamp=datetime.datetime(2023, 8, 23, 12, 0)),
-
-        sensorData(sensor_uid="40104", value=4.35, timestamp=datetime.datetime(2023, 8, 23, 15, 0)),  #40104-> 4.35
-        sensorData(sensor_uid="40104", value=3.50, timestamp=datetime.datetime(2022, 8, 23, 12, 0)),
-
-        sensorData(sensor_uid="10102", value=15.95, timestamp=datetime.datetime(2023, 8, 23, 16, 0)),  #10102-> 15.95
-        sensorData(sensor_uid="10102", value=18.71, timestamp=datetime.datetime(2023, 8, 23, 10, 0)),
-
-        sensorData(sensor_uid="40101", value=30.15, timestamp=datetime.datetime(2023, 8, 23, 17, 0)),  #40101-> 30.15
-        sensorData(sensor_uid="40101", value=29.52, timestamp=datetime.datetime(2023, 8, 22, 12, 0)),
-        sensorData(sensor_uid="40101", value=41.25, timestamp=datetime.datetime(2022, 3, 13, 11, 0)),
-
-        sensorData(sensor_uid="10101", value=3.75, timestamp=datetime.datetime(2023, 8, 28, 13, 0)),  #10101-> 3.75
-        sensorData(sensor_uid="10101", value=2.92, timestamp=datetime.datetime(2023, 8, 20, 10, 0)),
-        sensorData(sensor_uid="10101", value=1.05, timestamp=datetime.datetime(2022, 3, 19, 12, 0)),
-
-        sensorData(sensor_uid="20104", value=10.25, timestamp=datetime.datetime(2023, 7, 23, 11, 0)),  #20104-> 10.25
-        sensorData(sensor_uid="20104", value=21.55, timestamp=datetime.datetime(2022, 5, 31, 13, 0)),
-        sensorData(sensor_uid="20104", value=19.33, timestamp=datetime.datetime(2022, 8, 20, 10, 0))
-    ]
-    """
+"""
     
 
 def insert_dummy_data(db_instance,data_insert):
@@ -112,7 +85,7 @@ if __name__ == "__main__":
     db_instance.connect()
 
     clean_database()
-    new_data=generate_dummy_data(200)
+    new_data=generate_dummy_data(100)
     insert_dummy_data(db_instance,new_data)
 
     db_instance.disconnect()
