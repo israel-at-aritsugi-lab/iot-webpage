701f0fd220d9327c80880edf66161875f011a403
Angeline0210 <angelinechong0210@gmail.com>
2023-09-04 11:33:59 +0900

    edited the template and created status function

--- database_script.py
+++ database_script.py
@@ -1,6 +1,7 @@
 from angelinedb import database
 import mongoengine
-import datetime
+#import datetime
+from datetime import datetime
 
 class sensorData(mongoengine.Document):
     sensor_uid = mongoengine.StringField()
@@ -45,6 +46,39 @@ class Database:  ##
     def get_all_data_for_sensor(self,sensor_uid):  ##
          all_data_for_sensor=sensorData.objects(sensor_uid=sensor_uid).order_by('-timestamp')
          return all_data_for_sensor
+    
+    """
+    def check_sensor_status(self,sensor_uid):
+         all_data=self.get_all_data_for_sensor(sensor_uid)
+         if len(all_data)>=2:
+              latest_data=all_data[0]
+              second_latest_data=all_data[1]
+              time_difference_seconds=(latest_data.timestamp-second_latest_data.timestamp).total_seconds()
+              days_to_seconds=3*24*60*60
+              if time_difference_seconds>days_to_seconds:
+                return "NOT WORKING WELL !!!"
+              if time_difference_seconds not in [29,30,31]:
+                  return "NOT WORKING WELL !!!"
+              return "OK"
+         else:
+             return "insufficient data"
+    """
+
+    def check_sensor_status(self, sensor_uid, max_days_no_data=2):
+        latest_data = self.get_sensor_data_by_uid(sensor_uid)
+
+        if latest_data:
+            current_time = datetime.now()
+            if (current_time - latest_data.timestamp).days > max_days_no_data:
+                status = 'No Data'
+            else:
+                status = 'OK'
+        else:
+            status = 'No Data'
+
+        return status
+         
+         
 
 
     #def get_latest_sensor_data(self):
@@ -66,3 +100,12 @@ if __name__ == "__main__":
     retrieve_dummy_data(db_instance)  
 
     db_instance.disconnect()
+
+
+    #all_sensor_uids = db.get_all_sensor_data()  # Get all distinct sensor UIDs
+    #for sensor_uid in all_sensor_uids:
+    #    status = db.check_sensor_status(sensor_uid)
+        
+
+
+    
--- views.py
+++ views.py
@@ -9,7 +9,7 @@ from angelinedb import database  ##
 db_instance = database(host="127.0.0.1", port=27017, username="angeline", password="0000", db="my_db")  ##
 db_instance.connect()  ##
 
-
+"""
 #display all latest data for each sensor
 @views.route('/')
 def index():
@@ -18,7 +18,38 @@ def index():
     #latest_sensor_data = db.get_latest_sensor_data()
     #latest_data_date_time=db.get_latest_data_date_time()
     #return render_template('index.html', latest_data=latest_sensor_data,latest_date_time=latest_data_date_time)
-    return render_template('index.html', all_sensor_data=all_sensor_data)
+
+    #return render_template('index.html', all_sensor_data=all_sensor_data)
+
+
+    all_sensor_data_with_status = []
+    for data in all_sensor_data:
+        status = db.check_sensor_status(data.sensor_uid)
+        #all_sensor_data_with_status.append({"data": data, "status": status})
+        data_dict = {
+        "sensor_uid": data.sensor_uid,
+        "value": data.value,
+        "timestamp": data.timestamp,
+        "status": status
+        }   
+    all_sensor_data_with_status.append(data_dict)
+    return render_template('index.html', all_sensor_data_with_status=all_sensor_data_with_status)
+"""
+    
+@views.route('/')
+def index():
+    db = Database(db_instance)
+    all_sensor_data = db.get_all_sensor_data()
+    
+    sensor_status = {}  # Create a dictionary to store sensor statuses
+    
+    for data in all_sensor_data:
+        sensor_uid = data.sensor_uid
+        status = db.check_sensor_status(sensor_uid)
+        sensor_status[sensor_uid] = status
+    
+    return render_template('index.html', all_sensor_data=all_sensor_data, sensor_status=sensor_status)
+
 
 #db_instance.disconnect()  ##
 
