9af8a098bc4f864ffd418f329b931ebe4d1ac8f9
Angeline0210 <angelinechong0210@gmail.com>
2023-09-14 16:06:59 +0900

    changed file name & pass 1 parameter

--- views.py
+++ views.py
@@ -71,7 +71,7 @@ def index():
     all_sensor_data_json = json.dumps(all_sensor_data, default=datetime_serializer)
     
     
-    return render_template('index.html', all_sensor_data=all_sensor_data, sensor_status=sensor_status,all_sensor_data_json=all_sensor_data_json)
+    return render_template('homePage.html', all_sensor_data=all_sensor_data, sensor_status=sensor_status,all_sensor_data_json=all_sensor_data_json)
     #return render_template('index.html', all_sensor_data=all_sensor_data, sensor_status=sensor_status)
 
 #db_instance.disconnect()  
@@ -92,5 +92,5 @@ def display_sensor_data(sensor_uid):
 def display_all_data(sensor_uid):
     db=Database(db_instance)
     all_data_for_sensor=db.get_all_data_for_sensor(sensor_uid)
-    return render_template('index3.html',all_sensor_data=all_data_for_sensor)
+    return render_template('allDataPage.html',all_sensor_data=all_data_for_sensor)
    
